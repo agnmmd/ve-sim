6c4f19ca85ff15f7ec4568265de08dfde4218bff
Agon Memedi <memedi@ccs-labs.org>
2024-06-23 19:34:28 +0200

    Simplify stats

--- stats.py
+++ stats.py
@@ -6,24 +6,25 @@ class Statistics:
     _initialized = False
 
     @classmethod
-    def _initialize_files(cls):
+    def _initialize_file(cls, filename, fieldnames):
         directory = 'results'
         os.makedirs(directory, exist_ok=True)
-
-        # Initialize task statistics file
-        task_filename = os.path.join(directory, 'task_statistics.csv')
-        with open(task_filename, 'w', newline='') as csvfile:
-            fieldnames = ['Task ID', 'Source Car ID', 'Time of Arrival', 'Deadline', 'Priority', 'Complexity', 'Processing Start', 'Processing End', 'Processing Car', 'Repetition', 'Policy', 'Run']
+        filepath = os.path.join(directory, filename)
+        with open(filepath, 'w', newline='') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
 
-        # Initialize car statistics file
-        car_filename = os.path.join(directory, 'car_statistics.csv')
-        with open(car_filename, 'w', newline='') as csvfile:
-            fieldnames = ['Car ID', 'Generated Tasks', 'Processed Tasks', 'Successful Tasks', 'Total Processing Time', 'Lifetime']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-
+    @classmethod
+    def _initialize_files(cls):
+        cls._initialize_file('task_statistics.csv', [
+            'Task ID', 'Source Car ID', 'Time of Arrival', 'Deadline', 'Priority', 
+            'Complexity', 'Processing Start', 'Processing End', 'Processing Car', 
+            'Repetition', 'Policy', 'Run'
+        ])
+        cls._initialize_file('car_statistics.csv', [
+            'Car ID', 'Generated Tasks', 'Processed Tasks', 'Successful Tasks', 
+            'Total Processing Time', 'Lifetime', 'Repetition', 'Policy', 'Run'
+        ])
         cls._initialized = True
 
     @classmethod
@@ -32,49 +33,55 @@ class Statistics:
             cls._initialize_files()
 
     @staticmethod
-    def save_task_stats(task, processing_car_id):
+    def _save_stats(filename, fieldnames, data):
         Statistics._check_initialization()
-        directory = 'results'
-        filename = os.path.join(directory, 'task_statistics.csv')
-
-        with open(filename, 'a', newline='') as csvfile:
-            fieldnames = ['Task ID', 'Source Car ID', 'Time of Arrival', 'Deadline', 'Priority', 'Complexity', 'Processing Start', 'Processing End', 'Processing Car', 'Repetition', 'Policy', 'Run']
+        filepath = os.path.join('results', filename)
+        with open(filepath, 'a', newline='') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writerow({
-                'Task ID': task.id,
-                'Source Car ID': task.source_car.id,
-                'Time of Arrival': task.time_of_arrival,
-                'Deadline': task.deadline,
-                'Priority': task.priority,
-                'Complexity': task.complexity,
-                'Processing Start': task.processing_start,
-                'Processing End': task.processing_end,
-                'Processing Car': processing_car_id,
-                'Repetition': Sim.repetition,
-                'Policy': Sim.policy_name,
-                'Run': Sim.run
-            })
+            writer.writerow(data)
+
+    @staticmethod
+    def save_task_stats(task, processing_car_id):
+        fieldnames = [
+            'Task ID', 'Source Car ID', 'Time of Arrival', 'Deadline', 'Priority', 
+            'Complexity', 'Processing Start', 'Processing End', 'Processing Car', 
+            'Repetition', 'Policy', 'Run'
+        ]
+        data = {
+            'Task ID': task.id,
+            'Source Car ID': task.source_car.id,
+            'Time of Arrival': task.time_of_arrival,
+            'Deadline': task.deadline,
+            'Priority': task.priority,
+            'Complexity': task.complexity,
+            'Processing Start': task.processing_start,
+            'Processing End': task.processing_end,
+            'Processing Car': processing_car_id,
+            'Repetition': Sim.repetition,
+            'Policy': Sim.policy_name,
+            'Run': Sim.run
+        }
+        Statistics._save_stats('task_statistics.csv', fieldnames, data)
 
     @staticmethod
     def save_car_stats(car, current_time):
-        Statistics._check_initialization()
-        directory = 'results'
-        filename = os.path.join(directory, 'car_statistics.csv')
+        fieldnames = [
+            'Car ID', 'Generated Tasks', 'Processed Tasks', 'Successful Tasks', 
+            'Total Processing Time', 'Lifetime', 'Repetition', 'Policy', 'Run'
+        ]
+        data = {
+            'Car ID': car.id,
+            'Generated Tasks': len(car.generated_tasks),
+            'Processed Tasks': car.processed_tasks_count,
+            'Successful Tasks': car.successful_tasks,
+            'Total Processing Time': car.total_processing_time,
+            'Lifetime': current_time - car.time_of_arrival,
+            'Repetition': Sim.repetition,
+            'Policy': Sim.policy_name,
+            'Run': Sim.run
+        }
+        Statistics._save_stats('car_statistics.csv', fieldnames, data)
 
-        with open(filename, 'a', newline='') as csvfile:
-            fieldnames = ['Car ID', 'Generated Tasks', 'Processed Tasks', 'Successful Tasks', 'Total Processing Time', 'Lifetime', 'Repetition', 'Policy', 'Run']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writerow({
-                'Car ID': car.id,
-                'Generated Tasks': len(car.generated_tasks),
-                'Processed Tasks': car.processed_tasks_count,
-                'Successful Tasks': car.successful_tasks,
-                'Total Processing Time': car.total_processing_time,
-                'Lifetime': current_time - car.time_of_arrival,
-                'Repetition': Sim.repetition,
-                'Policy': Sim.policy_name,
-                'Run': Sim.run
-            })
 
 # Example of usage:
 # At the beginning of the simulation, you do not need to call any initialization method explicitly.
