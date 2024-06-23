5ccfe05fed20c67e885a1436a09a3d374a64e11e
Agon Memedi <memedi@ccs-labs.org>
2024-06-23 02:06:33 +0200

    minor

--- main.py
+++ main.py
@@ -8,14 +8,6 @@ import traci
 import os
 import simpy
 
-def generate_cars(env, sim, scheduler):
-    while True:
-        new_car = Car(env, sim, scheduler)
-        print(f"New Car {new_car.id} added at time {env.now}")
-        new_car.generate_tasks_static(2)
-        yield env.timeout(1)
-        # yield Sim.env.timeout(random.expovariate(lambda_rate))
-
 # def generate_cars_by_traces(traces, scheduler, region_of_interest):
 #     xmin, ymin, xmax, ymax = region_of_interest
 #     for car_id in traces[Sim.env.now].items():
@@ -25,7 +17,15 @@ def generate_cars(env, sim, scheduler):
 #             new_car.generate_tasks_static(2)
 
 #     yield Sim.env.timeout(1)
-#     # TODO: only register car in scheduler, if is ROI and unregister if not in ROI
+
+def generate_cars(env, sim, scheduler):
+    while True:
+        new_car = Car(env, sim)
+        scheduler.register_static_car([new_car])
+        print(f"New Car {new_car.id} added at time {env.now}")
+        new_car.generate_tasks_static(2)
+        yield env.timeout(1)
+        # yield Sim.env.timeout(random.expovariate(lambda_rate))
 
 def just_a_timer(env):
     """Just a timer that progresses time until the simulation ends"""
@@ -34,35 +34,40 @@ def just_a_timer(env):
         yield env.timeout(1)
 
 def run_sim(policy_func, run=-1, repetition=-1):
+
     env = simpy.Environment()
     sim = Sim()
     Sim.update_sim_variables(run=run, repetition=repetition, policy_function=policy_func)
-    scheduler = Scheduler(env)
+
     traci_mgr = TraciManager(env, sim)
     # traci_mgr.set_rois([(-50, -10, 50, 10)])
     sumo_binary = "/usr/bin/sumo"
     sumo_cfg = os.path.join(os.path.dirname(__file__), 'SUMO', 'street.sumocfg')
     sumo_cmd = [sumo_binary, "-c", sumo_cfg] #, "--quit-on-end"]#, "--start"])
+    scheduler = Scheduler(env, traci_mgr)
 
     ##################################################
-    # NOTE: Static car insertion
-    car1 = Car(env, sim, scheduler)
-    car2 = Car(env, sim, scheduler)
-    car3 = Car(env, sim, scheduler)
-    # env.process(car1.generate_task())
-    # env.process(car2.generate_task())
-    car1.generate_tasks_static(2)
-    car2.generate_tasks_static(1)
-    env.process(scheduler.schedule_tasks(Policy.random))
+    # NOTE: Scenario 1: Static car insertion
+    # car1 = Car(env, sim)
+    # car2 = Car(env, sim)
+    # car3 = Car(env, sim)
+    # # env.process(car1.generate_tasks())
+    # # env.process(car2.generate_tasks())
+    # car1.generate_tasks_static(2)
+    # car2.generate_tasks_static(1)
+
+    # scheduler.register_static_car([car1,car2])
     ##################################################
 
     ##################################################
-    # # NOTE: Dynamic car insertion
+    # NOTE: Scenario 2: Dynamic car insertion
     # env.process(generate_cars(env, sim, scheduler))
     ##################################################
 
     ##################################################
-    # car = Car(env, sim, scheduler)
+    # NOTE: Scenario 3: Static cars and tasks
+    # car = Car(env, sim)
+    # scheduler.register_static_car([car])
     # t1 = Task(env, sim, car)
     # t2 = Task(env, sim, car)
     # car.generated_tasks = [t1, t2]
@@ -72,30 +77,24 @@ def run_sim(policy_func, run=-1, repetition=-1):
 
     # t2.complexity = 4
     # t2.deadline = 10
+    ##################################################
+    
+    ##################################################
+    # NOTE: Scenario 4: Static cars and TraCI cars
+    env.process(just_a_timer(env))
 
-    # env.process(scheduler.schedule_tasks_exhaust(Policy.random))
+    car1 = Car(env, sim)
+    scheduler.register_static_car([car1])
+    
+    traci.start(sumo_cmd)
+    env.process(traci_mgr.execute_one_time_step())
     ##################################################
     
-    ##################################################
-    # env.process(just_a_timer(env))
-    
-    # traci_mgr = TraciManager(env)
-    # sumo_binary = "/usr/bin/sumo-gui"
-    # sumo_cfg = os.path.join(os.path.dirname(__file__), 'SUMO', 'street.sumocfg')
-    # sumo_cmd = [sumo_binary, "-c", sumo_cfg] #, "--quit-on-end"]#, "--start"])
-    # traci.start(sumo_cmd)
-    
-    # env.process(traci_mgr.execute_one_time_step())
-    ##################################################
-    
-    env.run(until=30)  
+    # Start Scheduling
+    env.process(scheduler.schedule_tasks_exhaust(policy_func))
 
-    # Print metrics
-    for car in scheduler.cars:
-        print(f"Car {car.id} - Processed Tasks: {car.processed_tasks_count}; "
-              f"Successful Tasks: {car.successful_tasks}; "
-              f"Total Processing Time: {car.total_processing_time}; "
-              f"Lifetime: {env.now - car.time_of_arrival}")    
+    env.run(until=30)
+
     # Print statistics for static cars that haven't been removed by dwell time
     for car in scheduler.static_cars:
         car.finish()
