7f5b74a3aa86a0903fbfd73856ef705a424a8f37
Agon Memedi <memedi@ccs-labs.org>
2024-09-19 11:10:18 +0200

    Refactor TraciManager: Introduce update_subscriptions() and update_vehicle_data() methods to handle simulated vehicle management

--- traci_manager.py
+++ traci_manager.py
@@ -15,8 +15,6 @@ class TraciManager:
         self.end_time = end_time  # New variable to store the simulation end time
 
     def execute_one_time_step(self):
-        previous_vehicle_ids = set()
-
         while traci.simulation.getMinExpectedNumber() > 0 and self.env.now <= self.end_time:
             try:
                 traci.simulationStep()
@@ -24,7 +22,8 @@ class TraciManager:
                 print(f"Time in SimPy: {self.env.now}, Time in SUMO: {traci.simulation.getTime()}")
 
                 if self.env.now >= 4:
-                    self._process_vehicles(previous_vehicle_ids)
+                    self.update_subscriptions()
+                    self.update_vehicle_data()
 
             except traci.exceptions.TraCIException as e:
                 print(f"TraCI Exception: {e}")
@@ -42,48 +41,6 @@ class TraciManager:
         traci.close()
         print("TraCI disconnected")
 
-    def _process_vehicles(self, previous_vehicle_ids):
-        driving_vehicles = set(traci.vehicle.getIDList())
-
-        for vehicle_id in driving_vehicles:
-            self._process_single_vehicle(vehicle_id)
-
-        self._handle_left_vehicles(previous_vehicle_ids, driving_vehicles)
-        previous_vehicle_ids.update(driving_vehicles)
-
-    def _process_single_vehicle(self, vehicle_id):
-        traci.vehicle.subscribe(vehicle_id, (tc.VAR_SPEED, tc.VAR_POSITION))
-        subscription_results = traci.vehicle.getSubscriptionResults(vehicle_id)
-        speed = subscription_results[tc.VAR_SPEED]
-        position = subscription_results[tc.VAR_POSITION]
-        print(f"Vehicle: {vehicle_id}: speed={speed}, position={position}")
-
-        if not self.rois or self.in_roi(position, self.rois):
-            print(f"Vehicle: {vehicle_id} is in ROI!")
-            if vehicle_id not in self.subscribed_vehicles:
-                # Create new Car object for newly subscribed vehicles
-                self.subscribed_vehicles[vehicle_id] = Car(self.env, self.sim, speed=speed, position=position)
-                self.subscribed_vehicles[vehicle_id].generate_tasks_static(10)
-                # self.env.process(self.subscribed_vehicles[vehicle_id].generate_tasks())
-            else:
-                # Update existing Car object
-                self.subscribed_vehicles[vehicle_id].update(speed=speed, position=position)
-        elif vehicle_id in self.subscribed_vehicles:
-            # Remove vehicle from subscriptions if it's no longer in ROI
-            del self.subscribed_vehicles[vehicle_id]
-
-    def _handle_left_vehicles(self, previous_vehicle_ids, driving_vehicles):
-        vehicles_left = previous_vehicle_ids - driving_vehicles
-        if vehicles_left:
-            print(f"Vehicles that left the simulation at {self.env.now}: {vehicles_left}")
-            for vehicle_id in vehicles_left:
-                if vehicle_id in self.subscribed_vehicles:
-                    self.subscribed_vehicles[vehicle_id].finish()
-                    del self.subscribed_vehicles[vehicle_id]
-
-        print("Vehicles that we care about, subscribed vehicles:", self.subscribed_vehicles.keys())
-        print("")
-
     def _is_in_roi(self, point):
         x, y = point
         return any(min_x <= x <= max_x and min_y <= y <= max_y for min_x, min_y, max_x, max_y in self.rois)
@@ -93,3 +50,71 @@ class TraciManager:
 
     def get_subscribed_vehicles_list(self):
         return list(self.subscribed_vehicles.values())
+
+    def update_subscriptions(self):
+        traci_vehicles = set(traci.vehicle.getIDList())
+
+        # self.remove_unsubscribed_vehicles(traci_vehicles)
+
+        if self.rois:
+            self.update_subscriptions_with_roi(traci_vehicles)
+        else:
+            self.update_subscriptions_without_roi(traci_vehicles)
+
+    def update_subscriptions_with_roi(self, traci_vehicles):
+        for vehicle_id in traci_vehicles:
+            if vehicle_id not in self.subscribed_vehicles:
+                position = traci.vehicle.getPosition(vehicle_id)
+                if self._is_in_roi(position):
+                    self.subscribe_to_vehicle(vehicle_id)
+
+        # Remove vehicles
+        for vehicle_id in list(self.subscribed_vehicles.keys()):
+            position = traci.vehicle.getPosition(vehicle_id)
+            if not self._is_in_roi(position):
+                self._handle_left_vehicles(vehicle_id, traci_vehicles)
+
+    def update_subscriptions_without_roi(self, traci_vehicles):
+        for vehicle_id in traci_vehicles:
+            if vehicle_id not in self.subscribed_vehicles:
+                self.subscribe_to_vehicle(vehicle_id)
+
+        # Remove vehicles
+        vehicles_to_remove = set(self.subscribed_vehicles.keys()) - traci_vehicles
+        for vehicle_id in vehicles_to_remove:
+            self._handle_left_vehicles(vehicle_id, traci_vehicles)
+
+    def subscribe_to_vehicle(self, vehicle_id):
+        traci.vehicle.subscribe(vehicle_id, [tc.VAR_POSITION, tc.VAR_SPEED])
+        self.subscribed_vehicles[vehicle_id] = Car(self.env, self.sim, speed=None, position=None)
+        self.subscribed_vehicles[vehicle_id].generate_tasks_static(14)
+        # self.env.process(self.subscribed_vehicles[vehicle_id].generate_tasks())
+
+    def remove_unsubscribed_vehicles(self, traci_vehicles):
+    #     vehicles_to_remove = set(self.subscribed_vehicles.keys()) - traci_vehicles
+    #     print("\tVehicles_to_remove:", vehicles_to_remove)
+    #     for vehicle_id in vehicles_to_remove:
+    #         self._handle_left_vehicles(vehicle_id, traci_vehicles)
+
+    #     if self.rois:
+    #         for vehicle_id in list(self.subscribed_vehicles.keys()):
+    #             position = traci.vehicle.getPosition(vehicle_id)
+    #             if not self._is_in_roi(position):
+    #                 print("\tVehilce left ROI:", vehicle_id)
+    #                 self._handle_left_vehicles(vehicle_id, traci_vehicles)
+        pass
+
+    def update_vehicle_data(self):
+        for vehicle_id in self.subscribed_vehicles:
+            results = traci.vehicle.getSubscriptionResults(vehicle_id)
+            if results:
+                position = results[tc.VAR_POSITION]
+                speed = results[tc.VAR_SPEED]
+                self.subscribed_vehicles[vehicle_id].update(speed=speed, position=position)
+                print(f"Vehicle: {vehicle_id}, Position: {position}, Speed: {speed}")
+
+    def _handle_left_vehicles(self, vehicle_id, traci_vehicles):
+        if vehicle_id in traci_vehicles:
+            traci.vehicle.unsubscribe(vehicle_id)
+        self.subscribed_vehicles[vehicle_id].finish()
+        del self.subscribed_vehicles[vehicle_id]
