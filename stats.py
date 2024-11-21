813c13be7a36becbd85647f6e2afb49994dc6923
Agon Memedi <memedi@ccs-labs.org>
2024-11-21 16:08:22 +0100

    Dynamically generate the output file names based on info from the Input Manager

--- stats.py
+++ stats.py
@@ -1,27 +1,46 @@
 import csv
 import os
 from sim import Sim
+from input_manager import InputManager
 
 class Statistics:
     _initialized = False
     _files = {
-        'task_statistics.csv': [
+        'task' :  [
             'Task ID', 'Source Car ID', 'Time of Arrival', 'Deadline', 'Priority',
             'Complexity', 'Status', 'Processing Car', 'Processing Start', 'Processing End',
             'Repetition', 'Policy', 'Run'
         ],
-        'car_statistics.csv': [
+        'car' : [
             'Car ID', 'Generated Tasks', 'Processed Tasks', 'Successful Tasks',
             'Queued Tasks', 'Processing power' ,'Total Processing Time', 'Lifetime', 'Repetition', 'Policy', 'Run'
         ]
     }
 
+    @staticmethod
+    def _filename(stat_type):
+        filename = [
+        f"_r_{InputManager.parsed_args.run}",
+        f"_p_{InputManager.parsed_args.policy}" if InputManager.parsed_args.policy else "",
+        f"_rep_{InputManager.parsed_args.repetition}" if InputManager.parsed_args.repetition else "",
+        f"_cf_{InputManager.parsed_args.configfile}" if InputManager.parsed_args.configfile else "",
+        f"_c_{InputManager.parsed_args.configurations}" if InputManager.parsed_args.configurations else "",
+        f"_tg_{InputManager.parsed_args.task_generation}" if InputManager.parsed_args.task_generation else "",
+        f"_s_{InputManager.parsed_args.start}" if InputManager.parsed_args.start else "",
+        f"_dur_{InputManager.parsed_args.duration}" if InputManager.parsed_args.duration else "",
+        f"_tc_{InputManager.parsed_args.task_complexity}" if InputManager.parsed_args.task_complexity else "",
+        f"_tp_{InputManager.parsed_args.task_priority}" if InputManager.parsed_args.task_priority else "",
+        f"_td_{InputManager.parsed_args.task_deadline}" if InputManager.parsed_args.task_deadline else "",
+        f"_cp_{InputManager.parsed_args.car_processing_power}" if InputManager.parsed_args.car_processing_power else ""
+    ]
+        return f"{stat_type}" + "".join(name for name in filename if name) + ".csv"
+
     @classmethod
     def _initialize_files(cls):
         directory = 'results'
         os.makedirs(directory, exist_ok=True)
-        for filename, fieldnames in cls._files.items():
-            filepath = os.path.join(directory, filename)
+        for stat_type, fieldnames in cls._files.items():
+            filepath = os.path.join(directory, Statistics._filename(stat_type))
             with open(filepath, 'w', newline='') as csvfile:
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter="\t")
                 writer.writeheader()
@@ -33,10 +52,10 @@ class Statistics:
             cls._initialize_files()
 
     @classmethod
-    def _save_stats(cls, filename, data):
+    def _save_stats(cls, stat_type, data):
         cls._check_initialization()
-        filepath = os.path.join('results', filename)
-        fieldnames = cls._files[filename]
+        filepath = os.path.join('results', Statistics._filename(stat_type))
+        fieldnames = cls._files[stat_type]
         with open(filepath, 'a', newline='') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter="\t")
             writer.writerow(data)
@@ -58,7 +77,7 @@ class Statistics:
             'Policy': Sim.policy_name,
             'Run': Sim.run
         }
-        cls._save_stats('task_statistics.csv', data)
+        cls._save_stats('task', data)
 
     @classmethod
     def save_car_stats(cls, car, current_time):
@@ -75,4 +94,4 @@ class Statistics:
             'Policy': Sim.policy_name,
             'Run': Sim.run
         }
-        cls._save_stats('car_statistics.csv', data)
+        cls._save_stats('car', data)
