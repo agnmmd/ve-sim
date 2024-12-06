c600a17ae38e0b041ce62030d616d3bbbe7d67b7
Agon Memedi <memedi@ccs-labs.org>
2024-12-06 16:04:31 +0100

    Changes to __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,12 @@
 # simulator/__init__.py
 
-from .sim import Sim
-from .task import Task
-from .car import Car
-from .scheduler import Scheduler
-from .sumo_manager import SumoManager
-from .traci_manager import TraciManager
-from .utils import print_color
-from .policy import example_policy
-from .stats import Statistics
+from sim import Sim
+from task import Task
+from car import Car
+from scheduler import Scheduler
+from sumo_manager import SumoManager
+from traci_manager import TraciManager
+from utils import print_color
+from policy import Policy
+from stats import Statistics
+from input_manager import InputManager
