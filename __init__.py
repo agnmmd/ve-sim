8bc0de7c9ffe88f636212edc956005e8cd3d4fb0
Agon Memedi <memedi@ccs-labs.org>
2025-03-26 17:32:21 +0100

    Minor

--- __init__.py
+++ __init__.py
@@ -4,7 +4,6 @@ from sim import Sim
 from task import Task
 from car import Car
 from scheduler import Scheduler
-from sumo_manager import SumoManager
 from traci_manager import TraciManager
 from utils import print_color
 from policy import Policy
